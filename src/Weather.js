c8ab44d3b2baf4d8e3742691f8e33dd70c4a5d57
Stella5791 <142344371+Stella5791@users.noreply.github.com>
2024-09-12 11:44:50 -0300

    added formatted date component

--- src/Weather.js
+++ src/Weather.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  const apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3";
-  let city = "London";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+  function search() {
+    const apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  if (weatherData.ready){
   return (
     <div className="Weather ">
-      <form className="search-form">
+      <form onSubmnit={handleSubmit}>
         <div class="input-group mb-3">
           <input
             type="search"
             placeholder="Enter a city"
             className="form-control"
+            autofocus="on"
+            onChange={handleCityChange}
             aria-describedby="button-addon2"
           />
           <button
@@ -26,6 +52,15 @@ export default function Weather() {
           </button>
         </div>
       </form>
+      <WeatherInfo data={weatherData}/>
+      <WeatherForecast coordinates={weatherData.coordinates}/>
+;
+    else{
+search ( );
+return "Loading...";
+    }
+    
+      
       <hr />
       <div className="overview">
         <h1 className="place text-left">Cordoba</h1>
@@ -63,4 +98,5 @@ export default function Weather() {
       <hr />
     </div>
   );
+}
 }
