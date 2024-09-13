30dba2a79fbcdf24705137fadad1583354a47e3a
Stella5791 <142344371+Stella5791@users.noreply.github.com>
2024-09-13 09:32:34 -0300

    added weather info and date

--- src/FormatedDate.js
+++ src/FormatedDate.js
@@ -16,6 +16,10 @@ export default function FormattedDate(props) {
   if (hours < 10) {
     hours = `0${minutes}`;
   }
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   return (
     <div>
       {" "}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,34 +7,34 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <div className="overview">
-        <h1 className="place text-left">Cordoba</h1>
+        <h1 className="place text-left">{props.data.city}</h1>
         <FormattedDate date={weatherData} />
         <div className="row overview-info">
           <div className="col-6 current-temperature">
             <div className=" d-flex temperature">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-                alt="cloudy"
-              />
               <div className="d-flex">
-                <strong>xx</strong>
+                <strong>
+                  <WeatherIcon code={props.data.icon} size={52} />
+                </strong>
                 <span className="units">
-                  <a href="/">°C</a> | <a href="/">°F</a>
+                  <WeatherTemperature celsius={props.data.temperature} />
                 </span>
               </div>
             </div>
           </div>
           <div className="col-6 pb-3 info">
             <p>
-              <div>Last updated: 4520</div>
+              <div>
+                <FormattedDate date={props.data.date} />
+              </div>
               <div>
                 <strong>
                   {" "}
-                  <em>Cloudy</em>{" "}
+                  <em>{props.data.description}</em>{" "}
                 </strong>
               </div>
-              <div>Humidity:%</div>
-              <div>Wind: km/h</div>
+              <div>Humidity: {props.data.humidity}%</div>
+              <div>Wind: {props.data.wind}km/h</div>
             </p>
           </div>
         </div>
